670769aa4a152a607264b336b8323581bced3be7
m7rlin <stawowczyk.martin@gmail.com>
2019-09-19 19:25:54 +0200

    Add MyFirstWindows project
    Button that flies in all directions and bounce agains a wall.

---
+++ src/MyFirstWindows/MyFirstWindows/FormMain.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MyFirstWindows
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonUFO = new System.Windows.Forms.Button();
+            this.timer10ms = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonUFO
+            // 
+            this.buttonUFO.Location = new System.Drawing.Point(43, 89);
+            this.buttonUFO.Name = "buttonUFO";
+            this.buttonUFO.Size = new System.Drawing.Size(120, 46);
+            this.buttonUFO.TabIndex = 0;
+            this.buttonUFO.Text = "UFO";
+            this.buttonUFO.UseVisualStyleBackColor = true;
+            // 
+            // timer10ms
+            // 
+            this.timer10ms.Enabled = true;
+            this.timer10ms.Tick += new System.EventHandler(this.timer10ms_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 396);
+            this.Controls.Add(this.buttonUFO);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUFO;
+        private System.Windows.Forms.Timer timer10ms;
+    }
+}
+
---
+++ src/MyFirstWindows/MyFirstWindows/FormMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstWindows
+{
+    public partial class FormMain : Form
+    {
+        /**
+         * ==== Bewegen
+         * 2 -- nach unten
+         * 8 -- nach oben
+         * 6 -- nach rechts
+         * 4 -- nach links
+         */
+        int iRichtung = 2;
+        int iButtonPosX = 100;
+        int iButtonPosY = 100;
+
+        int iGeschwindigkeit = 5;
+
+        Random zufall = new Random();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void timer10ms_Tick(object sender, EventArgs e)
+        {
+            int iUFOX = buttonUFO.Location.X;
+            int iUFOY = buttonUFO.Location.Y;
+
+            if (iUFOY >= 250 || iUFOY <= 50 || iUFOX <= 50 || iUFOX >= 250)
+            {
+                iRichtung = zufall.Next(1, 8);
+            }
+
+            switch (iRichtung)
+            {
+                case 2:
+                    // nach unten Bewegen
+                    buttonUFO.Top += iGeschwindigkeit;
+                    break;
+
+                case 6:
+                    // nach rechts Bewegen
+                    buttonUFO.Left += iGeschwindigkeit;
+                    break;
+
+
+                case 4:
+                    // nach links Bewegen
+                    buttonUFO.Left -= iGeschwindigkeit;
+                    break;
+
+                case 8:
+                    // nach oben Bewegen
+                    buttonUFO.Top -= iGeschwindigkeit;
+                    break;
+                case 7:
+                    // nach links oben Bewegen
+                    buttonUFO.Left -= iGeschwindigkeit;
+                    buttonUFO.Top -= iGeschwindigkeit;
+                    break;
+
+                case 1:
+                    // nach links unten Bewegen
+                    buttonUFO.Left -= iGeschwindigkeit;
+                    buttonUFO.Top += iGeschwindigkeit;
+                    break;
+                case 3:
+                    // nach rechts unten Bewegen
+                    buttonUFO.Left += iGeschwindigkeit;
+                    buttonUFO.Top += iGeschwindigkeit;
+                    break;
+
+                case 5:
+                    // nach rechts oben Bewegen
+                    buttonUFO.Left += iGeschwindigkeit;
+                    buttonUFO.Top -= iGeschwindigkeit;
+                    break;
+
+            }
+            
+        }
+    }
+}
