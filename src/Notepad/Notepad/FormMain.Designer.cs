437f2b6be72b67b6b0b0452287a0d51791780915
m7rlin <stawowczyk.martin@gmail.com>
2019-11-21 19:00:06 +0100

    Add save as function

--- src/Notepad/Notepad/FormMain.Designer.cs
+++ src/Notepad/Notepad/FormMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStripMain = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,7 +37,17 @@
             this.toolStripMenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox = new System.Windows.Forms.TextBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelZeit = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelDatum = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1s = new System.Windows.Forms.Timer(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemSize = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.menuStripMain.SuspendLayout();
+            this.statusStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStripMain
@@ -44,7 +55,8 @@
             this.menuStripMain.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItemFile,
-            this.toolStripMenuItemHelp});
+            this.toolStripMenuItemHelp,
+            this.editToolStripMenuItem});
             this.menuStripMain.Location = new System.Drawing.Point(0, 0);
             this.menuStripMain.Name = "menuStripMain";
             this.menuStripMain.Size = new System.Drawing.Size(716, 28);
@@ -64,14 +76,14 @@
             // ToolStripMenuItemOpen
             // 
             this.ToolStripMenuItemOpen.Name = "ToolStripMenuItemOpen";
-            this.ToolStripMenuItemOpen.Size = new System.Drawing.Size(181, 26);
+            this.ToolStripMenuItemOpen.Size = new System.Drawing.Size(142, 26);
             this.ToolStripMenuItemOpen.Text = "Open";
             this.ToolStripMenuItemOpen.Click += new System.EventHandler(this.ToolStripMenuItemOpen_Click);
             // 
             // ToolStripMenuItemSave
             // 
             this.ToolStripMenuItemSave.Name = "ToolStripMenuItemSave";
-            this.ToolStripMenuItemSave.Size = new System.Drawing.Size(181, 26);
+            this.ToolStripMenuItemSave.Size = new System.Drawing.Size(142, 26);
             this.ToolStripMenuItemSave.Text = "Save";
             this.ToolStripMenuItemSave.Click += new System.EventHandler(this.ToolStripMenuItemSave_Click);
             // 
@@ -80,12 +92,14 @@
             this.ToolStripMenuItemSaveAs.Name = "ToolStripMenuItemSaveAs";
             this.ToolStripMenuItemSaveAs.Size = new System.Drawing.Size(181, 26);
             this.ToolStripMenuItemSaveAs.Text = "Save as...";
+            this.ToolStripMenuItemSaveAs.Click += new System.EventHandler(this.ToolStripMenuItemSaveAs_Click);
             // 
             // toolStripMenuItemHelp
             // 
             this.toolStripMenuItemHelp.Name = "toolStripMenuItemHelp";
             this.toolStripMenuItemHelp.Size = new System.Drawing.Size(53, 24);
             this.toolStripMenuItemHelp.Text = "Help";
+            this.toolStripMenuItemHelp.Click += new System.EventHandler(this.toolStripMenuItemHelp_Click);
             // 
             // textBox
             // 
@@ -95,24 +109,99 @@
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(716, 416);
             this.textBox.TabIndex = 2;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // openFileDialog
             // 
             this.openFileDialog.DefaultExt = "txt";
             this.openFileDialog.FileName = "test";
+            this.openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelZeit,
+            this.toolStripStatusLabelDatum});
+            this.statusStrip.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(716, 25);
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelZeit
+            // 
+            this.toolStripStatusLabelZeit.AutoSize = false;
+            this.toolStripStatusLabelZeit.Name = "toolStripStatusLabelZeit";
+            this.toolStripStatusLabelZeit.Size = new System.Drawing.Size(200, 20);
+            this.toolStripStatusLabelZeit.Text = "toolStripStatusLabelZeit";
+            this.toolStripStatusLabelZeit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabelDatum
+            // 
+            this.toolStripStatusLabelDatum.AutoSize = false;
+            this.toolStripStatusLabelDatum.Name = "toolStripStatusLabelDatum";
+            this.toolStripStatusLabelDatum.Size = new System.Drawing.Size(200, 20);
+            this.toolStripStatusLabelDatum.Text = "toolStripStatusLabelDatum";
+            this.toolStripStatusLabelDatum.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer1s
+            // 
+            this.timer1s.Enabled = true;
+            this.timer1s.Tick += new System.EventHandler(this.timer1s_Tick);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFont});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // toolStripMenuItemFont
+            // 
+            this.toolStripMenuItemFont.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemSize,
+            this.toolStripMenuItemColor});
+            this.toolStripMenuItemFont.Name = "toolStripMenuItemFont";
+            this.toolStripMenuItemFont.Size = new System.Drawing.Size(181, 26);
+            this.toolStripMenuItemFont.Text = "Font";
+            // 
+            // toolStripMenuItemSize
+            // 
+            this.toolStripMenuItemSize.Name = "toolStripMenuItemSize";
+            this.toolStripMenuItemSize.Size = new System.Drawing.Size(181, 26);
+            this.toolStripMenuItemSize.Text = "Size";
+            this.toolStripMenuItemSize.Click += new System.EventHandler(this.toolStripMenuItemSize_Click);
+            // 
+            // toolStripMenuItemColor
+            // 
+            this.toolStripMenuItemColor.Name = "toolStripMenuItemColor";
+            this.toolStripMenuItemColor.Size = new System.Drawing.Size(181, 26);
+            this.toolStripMenuItemColor.Text = "Color";
+            this.toolStripMenuItemColor.Click += new System.EventHandler(this.toolStripMenuItemColor_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "test";
+            this.saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(716, 444);
+            this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.menuStripMain);
             this.MainMenuStrip = this.menuStripMain;
             this.Name = "FormMain";
             this.Text = "Notepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.menuStripMain.ResumeLayout(false);
             this.menuStripMain.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +216,15 @@
         private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemSaveAs;
         private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelZeit;
+        private System.Windows.Forms.Timer timer1s;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelDatum;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFont;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSize;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemColor;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
--- src/Notepad/Notepad/FormMain.cs
+++ src/Notepad/Notepad/FormMain.cs
@@ -12,9 +12,23 @@ namespace Notepad
 {
     public partial class FormMain : Form
     {
+
+        // File changed
+        bool bFileChanged = false;
+
+        bool bFileExist = false;
+
+
+        string sFileName = "<unknown>";
+
         public FormMain()
         {
             InitializeComponent();
+            timer1s_Tick(null, null);
+
+
+            Text = sFileName;
+            
         }
            
         // Open - Menu 
@@ -23,13 +37,112 @@ namespace Notepad
            if (DialogResult.OK == openFileDialog.ShowDialog())
             {
                 textBox.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+                bFileExist = true;
             }
         }
 
         // Save - Menu
         private void ToolStripMenuItemSave_Click(object sender, EventArgs e)
         {
-            System.IO.File.WriteAllText(openFileDialog.FileName, textBox.Text);
+            this.saveFile();
+        }
+
+        private void timer1s_Tick(object sender, EventArgs e)
+        {
+            DateTime oLocalTime = DateTime.Now;
+
+            toolStripStatusLabelZeit.Text = oLocalTime.ToLongTimeString();
+            toolStripStatusLabelDatum.Text = oLocalTime.ToLongDateString();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (bFileChanged)
+            {
+                DialogResult oDialogResult = MessageBox.Show("Do you want to save before closing?", "Exit", MessageBoxButtons.YesNoCancel);
+                if (DialogResult.Yes == oDialogResult)
+                {
+                    // Clicked yes
+                    this.saveFile();
+                } if (DialogResult.Abort == oDialogResult) {
+                    // Clicked abort
+                    e.Cancel = true;
+                }
+                // Clicked no
+            }
+        }
+
+        private void saveAsFile()
+        {
+            saveFileDialog.ShowDialog();
+            System.IO.File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+        }
+
+        private void saveFile()
+        {
+            if (!bFileExist)
+            {
+                // Save file
+                try
+                {
+                    System.IO.File.WriteAllText(openFileDialog.FileName, textBox.Text);
+                }
+
+                catch
+                {
+                    MessageBox.Show("Speichern geht nit");
+                }
+            } else
+            {
+                this.saveAsFile();
+            }
+            bFileChanged = false;
+            bFileExist = true;
+        }
+
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            // File text changed
+            bFileChanged = true;
+
+            // If file already exist on the disk but the content has changed
+            // update filename with *
+            if (true)
+            {
+                this.Text = "*" + sFileName;
+            }
+        }
+
+        private void toolStripMenuItemHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("(c) 2019 — Marcin Stawowczyk");
+        }
+
+        // Set Editor font size
+        private void toolStripMenuItemSize_Click(object sender, EventArgs e)
+        {
+            FontDialog ftd = new FontDialog();
+            if (ftd.ShowDialog() == DialogResult.OK)
+            {
+                textBox.Font = ftd.Font;
+            }
+        }
+
+        // Set Editor font color
+        private void toolStripMenuItemColor_Click(object sender, EventArgs e)
+        {
+
+            ColorDialog ftd = new ColorDialog();
+            if (ftd.ShowDialog() == DialogResult.OK)
+            {
+                textBox.ForeColor = ftd.Color;
+            }
+        }
+
+        private void ToolStripMenuItemSaveAs_Click(object sender, EventArgs e)
+        {
+            this.saveAsFile();
         }
     }
 }
